0bab8cef2e1fea9be6f72146ed328de851f884ff
knanirj <raja.kif11@aluno.ifsc.edu.br>
2024-10-22 13:10:29 -0300

    dernier

--- SaláriocomBônus.py
+++ SaláriocomBônus.py
@@ -1,2 +1,12 @@
 # coding: utf-8
 # Exercício: Salário com Bônus
+def salbon(salario,totalvendas):
+    comissão= totalvendas * 0.15
+    totalareceber=salario+comissão
+    return(comissão,totalareceber)
+for i in range (3):
+    nomedevendeur=str(input("entrer le nom de vendeur:"))
+    salario=float(input("entrer le salaire fixe:"))
+    totalvendas=float(input("entrer le total de vente en real:"))
+    comissão,totalareceber=salbon(salario,totalvendas)
+    print(f"a comissão e{comissão},e o totalareceber e {totalareceber}, para vendedor : {nomedevendeur}")
